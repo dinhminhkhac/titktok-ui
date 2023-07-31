720cdd902272a3ee30cb5e89a0ec553cb6c492b5
Khac Dinh <dinhminhkhac.1503@gmail.com.vn>
2023-07-31 09:30:18 +0900

    build default layout frame

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,13 @@
+import classNames from 'classnames/bind'; //avaiable input className with x-y (e.g className ={styles.item-post})
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles);
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}></div>
+        </header>
+    );
 }
 
 export default Header;
